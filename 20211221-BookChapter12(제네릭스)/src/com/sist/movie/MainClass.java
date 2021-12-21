788ee7802a1420bace4d650fbe19a21b1362db4a
user <user@DESKTOP-COJ9858>
2021-12-21 15:53:07 +0900

    1111

--- 20211221-BookChapter12(제네릭스)/src/com/sist/movie/MainClass.java
+++ 20211221-BookChapter12(제네릭스)/src/com/sist/movie/MainClass.java
@@ -1,10 +1,69 @@
 package com.sist.movie;
 // 사용자 프로그램 (Front)
+import java.util.*;
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        MovieSystem ms=new MovieSystem();
+        MainClass mc=new MainClass();
+        while(true)
+        {
+        	int m=mc.menu();
+        	switch(m)
+        	{
+        	 case 1:
+        	 {
+        		ArrayList<Movie> list=ms.movieAllData();
+        		// 영화 전체 목록을 가지고 온다 
+        		for(Movie mm:list)
+        		{
+        			System.out.println(mm.getNo()+"."+mm.getTitle()+
+        					"("+mm.getScore()+")");
+        		}
+        	 }
+        	 break;
+        	case 2:
+        	{
+        		Scanner scan=new Scanner(System.in);
+        		System.out.print("상세볼 영화 선택(1~20):");
+        		int no=scan.nextInt();
+        		Movie mm=ms.movieDetailData(no);
+        		System.out.println("순위:"+mm.getNo());
+        		System.out.println("영화명:"+mm.getTitle());
+        		System.out.println("평점:"+mm.getScore());
+        		System.out.println("예매율:"+mm.getReserve());
+        		System.out.println("개봉일:"+mm.getRegdate());
+        		System.out.println(mm.getStory());
+        	}	
+        	break;
+        	case 3:
+        		Scanner scan=new Scanner(System.in);
+        		System.out.print("검색어 입력:");
+        		String title=scan.next();
+        		ArrayList<Movie> list=ms.movieFind(title);
+        		for(Movie mm:list)
+        		{
+        			System.out.println(mm.getTitle());
+        		}
+        		break;
+        	case 4:
+        		System.out.println("프로그램을 종료합니다!!");
+        		System.exit(0);// 정상 종료 
+        	}
+        }
+	}
+	public int menu()
+	{
+		System.out.println("===== 메뉴 =====");
+		System.out.println("1. 영화 목록 보기 ");
+		System.out.println("2. 영화 상세 보기 ");
+		System.out.println("3. 영화 찾기 ");
+		System.out.println("4. 종료");
+		System.out.println("===============");
+		Scanner scan=new Scanner(System.in);
+		System.out.print("메뉴 선택(1~4):");
+		return scan.nextInt();
 	}
 
 }
--- 20211221-BookChapter12(제네릭스)/src/com/sist/movie/MovieSystem.java
+++ 20211221-BookChapter12(제네릭스)/src/com/sist/movie/MovieSystem.java
@@ -31,17 +31,52 @@ public class MovieSystem {
 		   // 사이트 연결 => 데이터를 읽어서 저장 (Document:문서 저장)
 		   Document doc=Jsoup.connect("https://movie.daum.net/ranking/reservation").get();
 		   /*
-		    * private String title;
+		    *   private String title;
 			    private double score;
 			    private String reserve;
 			    private String story;
 			    private String regdate;
+			    <strong class="tit_item"> 
+<a href="/moviedb/main?movieId=146656" 
+  class="link_txt" data-tiara-layer="moviename">
+   스파이더맨: 노 웨이 홈</a></strong>
+   
+   <span class="txt_append"> <span class="info_txt">
+   평점<span class="txt_grade">8.2</span></span> 
+   
+   <span class="info_txt">예매율<span class="txt_num">68.4%</span></span> <
+/span> <span class="txt_info"> 개봉<span class="txt_num">21.12.15</span> 
+ * <a href="/moviedb/main?movieId=146656" class="link_story" data-tiara-layer="poster"> 영화 <스파이더맨: 노 웨이 홈>은 정체가 탄로난 스파이더맨 '피터 파커'(톰 홀랜드)가문제를 해결하기 위해 '닥터 스트레인지'(베네딕트 컴버배치)의 도움을 받던 중 뜻하지 않게 멀티버스가 열리게 되고,이를 통해 '닥터 옥토퍼스'(알프리드 몰리나) 등 각기 다른 차원의 숙적들이 나타나며 사상 최악의 위기를 맞게 되는 이야기를 그린 마블 액션 블록버스터. </a> 
+
+
 		    */
-		   Elements title=doc.select("");
-		   Elements score=doc.select("");
-		   Elements reserve=doc.select("");
-		   Elements story=doc.select("");
-		   Elements regdate=doc.select("");
+		   Elements title=doc.select("strong.tit_item a.link_txt");
+		   // Element => 태그 한개를 모아 준다 
+		   // Elements (Collection) => 같은 태그 여러개 
+		   Elements score=doc.select("span.txt_grade");//css selector =>UI
+		   Elements reserve=doc.select("span.info_txt span.txt_num");
+		   Elements story=doc.select("a.link_story");
+		   Elements regdate=doc.select("span.txt_info span.txt_num");
+		   for(int i=0;i<title.size();i++)
+		   {
+			   System.out.println(title.get(i).text());
+			   System.out.println(score.get(i).text());
+			   System.out.println(reserve.get(i).text());
+			   System.out.println(regdate.get(i).text());
+			   System.out.println(story.get(i).text());
+			   System.out.println("===================================");
+			   
+			   Movie m=new Movie();
+			   m.setNo(i+1);
+			   m.setTitle(title.get(i).text());
+			   m.setStory(story.get(i).text());
+			   m.setReserve(reserve.get(i).text());
+			   m.setRegdate(regdate.get(i).text());
+			   m.setScore(Double.parseDouble(score.get(i).text()));
+			   // 한개 영화 정보 => 20개 
+			   list.add(m);
+			   // 변수(여러개) => 클래스 묶는다 ==> 여러개 ==>  Collection
+		   }
 		   
 	   }catch(Exception ex)
 	   {
@@ -50,7 +85,40 @@ public class MovieSystem {
 	   
    }
    //1.종류별 목록 
-   //2.찾기 
+   // ArrayList<Movie> => 영화정보 전체
+   // Movie => 영화 한개 
+   public ArrayList<Movie> movieAllData()
+   {
+	   return list;
+   }
+   //2.상세보기 (영화한개에 대한 정보를 본다)
+   public Movie movieDetailData(int no)
+   {
+	   Movie m=new Movie();
+	   for(Movie mm:list)
+	   {
+		   if(mm.getNo()==no)
+		   {
+			   m=mm;
+			   break;// 찾은 경우에 종료하고 루프를 끝낸다 
+		   }
+	   }
+	   return m;
+   }
+   //3. 찾기 => 영화가 여러개 있는 경우도 있다 
+   public ArrayList<Movie> movieFind(String title)
+   {
+	   ArrayList<Movie> mList=new ArrayList<Movie>();
+	   // mList에 찾은 영화(Movie) 모아서 넘겨준다
+	   for(Movie m:list)
+	   {
+		   if(m.getTitle().contains(title))
+		   {
+			   mList.add(m);
+		   }
+	   }
+	   return mList;
+   }
 }
 
 

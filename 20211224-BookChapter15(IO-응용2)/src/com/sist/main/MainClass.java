5de36459d6ea08bdfd8bca2c4077a7fbb6e3cedb
user <user@DESKTOP-COJ9858>
2021-12-24 16:05:37 +0900

    1111

--- 20211224-BookChapter15(IO-응용2)/src/com/sist/main/MainClass.java
+++ 20211224-BookChapter15(IO-응용2)/src/com/sist/main/MainClass.java
@@ -1,10 +1,55 @@
 package com.sist.main;
-
+// 클라이언트 (브라우저) => 사용자 보는 곳 (Front-End)
+import java.util.*;
+import com.sist.music.*;
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        // 사용자 요청 => 해당페이지 요청 
+		Scanner scan=new Scanner(System.in);
+		MusicSystem ms=new MusicSystem();
+		int total=ms.musicTotalPage();
+		System.out.print("페이지 입력(1~"+total+"page):");
+		int page=scan.nextInt();
+		// 해당 페이지의 값을 받는다 
+		ArrayList<Music> list=ms.musicListData(page);
+		// 받은 데이터 출력 
+		System.out.println("===== "+page+"page 뮤직 목록=====");
+		for(Music m:list)
+		{
+			System.out.println(m.getNo()+"."+m.getTitle());
+		}
+		
+		System.out.println("===== 등폭이 가장 큰 뮤직 목록 =====");
+		list.clear();
+		list=ms.musicMaxData();
+		for(Music m:list)
+		{
+			System.out.println(m.getNo()+"."+m.getTitle()
+			           +"("+m.getIdcrment()+")");
+		}
+		
+		System.out.println("===== 제목으로 찾기 =====");
+		System.out.print("제목 입력:");
+		String title=scan.next();
+		list.clear();
+		list=ms.titleFindData(title);
+		for(Music m:list)
+		{
+			System.out.println(m.getNo()+"."+m.getTitle());
+		}
+		System.out.println("===== 가수명으로 찾기 =====");
+		System.out.print("가수 입력:");
+		String singer=scan.next();
+		list.clear();
+		list=ms.singerFindData(singer);
+		for(Music m:list)
+		{
+			System.out.println(m.getNo()+"."+m.getTitle()+
+					"("+m.getSinger()+")");
+		}
+		
 	}
 
 }
--- 20211224-BookChapter15(IO-응용2)/src/com/sist/music/MusicSystem.java
+++ 20211224-BookChapter15(IO-응용2)/src/com/sist/music/MusicSystem.java
@@ -1,5 +1,124 @@
 package com.sist.music;
-
+// 사용자 요청 => 처리후 응답 
+import java.util.*;
+import java.io.*;
 public class MusicSystem {
-
+   // 음악 정보 ==> 전체를 가지고 있다 (공유) => static 
+   // 음악 1 (Music) , 음악 전체 (ArrayList<Music>)
+   static private ArrayList<Music> list=
+		             new ArrayList<Music>();
+   // 초기화 => ArrayList에 값을 채운다 
+   static
+   {
+	   try
+	   {
+		   // 파일에 읽는다 => 객체단위로 변환 => ArrayList대입 
+		   FileInputStream fis=
+				   new FileInputStream("c:\\java_data\\music_object.txt");
+		   ObjectInputStream ois=
+				   new ObjectInputStream(fis);
+		   list=(ArrayList<Music>)ois.readObject();
+		   // 파일 닫기
+		   fis.close();
+		   ois.close();
+	   }catch(Exception ex) 
+	   {
+		   ex.printStackTrace();
+	   }
+   }
+   // 사용자 요청 처리 
+   // 목록 요청 
+   public ArrayList<Music> musicListData(int page)
+   {
+	   ArrayList<Music> mList=
+			   new ArrayList<Music>();
+	   int j=0;// 10개씩 잘라주는 변수 
+	   // 실전에 필요한 제어문 ==> 페이징 기법 => 오라클(인라인뷰)
+	   int pagecnt=(page*10)-10;
+	   /*
+	    *    1page ==> 0~9
+	    *    2page ==> 10~19
+	    *    3page ==> 20~29
+	    *    ..
+	    *    ..
+	    *    ..
+	    */
+	   for(int i=0;i<list.size();i++)
+	   {
+		   Music m=list.get(i);
+		   if(j<10 && i>=pagecnt)
+		   {
+			   mList.add(m);
+			   j++;
+		   }
+	   }
+	   return mList;
+   }
+   public int musicTotalPage()
+   {
+	   return (int)(Math.ceil(list.size()/10.0));
+	   // 총페이지를 구할때 => 올림메소드를 이용한다  => ceil()
+   }
+   // 제목 찾기 요청 
+   public ArrayList<Music> titleFindData(String title)
+   {
+	   ArrayList<Music> mList=new ArrayList<Music>();
+	   for(Music m:list)
+	   {
+		   if(m.getTitle().contains(title))
+		   {
+			   mList.add(m);
+		   }
+	   }
+	   return mList;
+   }
+   // 등폭이 가장 많은 음악 
+   public ArrayList<Music> musicMaxData()
+   {
+	   ArrayList<Music> mList=
+			   new ArrayList<Music>();
+	   // 등폭 확인 
+	   int max=0;
+	   for(Music m:list)
+	   {
+		   if(max<m.getIdcrment())
+			   max=m.getIdcrment();
+	   }
+	   
+	   for(Music m:list)
+	   {
+		   if(m.getIdcrment()==max)
+		   {
+			   mList.add(m);
+		   }
+	   }
+	   return mList;
+   }
+   // 가수명으로 찾기
+   public ArrayList<Music> singerFindData(String singer)
+   {
+	   ArrayList<Music> sList=new ArrayList<Music>();
+	   for(Music m:list)
+	   {
+		   if(m.getSinger().contains(singer))
+		   {
+			   sList.add(m);
+		   }
+	   }
+	   return sList;
+   }
+   // 상세 보기 
+   public Music musicDetailData(int no)//번호 => 1
+   {
+	   Music m=list.get(no-1); // index = 0
+	   return m;
+   }
 }
+
+
+
+
+
+
+
+
